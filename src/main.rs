371aceb202c4a43a04ce1bab4e62ef542c8967fd
Nahtan <nathan.titirangi@gmail.com>
2023-10-29 14:40:21 +1300

    it generates the bytecode

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,14 @@ struct FnDef {
 enum Instruction {
     SUB,
     DIV,
-    CALL,
+    CALL, // adr, argc, ...
     RET, // jump to last call
     CMP, // a, b => {-1,0,1} : {< = >}
     JZ,  // cond, adr
     ARR,
-	SET,
+    SET,
+    REF,
+    PRINT,
 }
 
 #[derive(Debug)]
@@ -35,7 +37,7 @@ enum ByteCodePoint {
     c(Instruction),
     f(f64),
     i(i64),
-	s(String),
+    s(String),
 }
 
 fn next_tok<'a>(tokens: &'a [String], ptr: &mut usize) -> &'a String {
@@ -50,47 +52,82 @@ fn draw(
     compiled: &mut Vec<ByteCodePoint>,
 ) {
     let tok = next_tok(tokens, ptr);
-    if tok == "fn" {
-        let fn_name = next_tok(tokens, ptr);
-        let argc = next_tok(tokens, ptr).parse::<usize>().unwrap();
-        compiled.push(ByteCodePoint::c(Instruction::JZ));
-        compiled.insert(compiled.len(), ByteCodePoint::i(0));
-        let target = compiled.len();
-        fns.last_mut().unwrap().insert(
-            fn_name.clone(),
-            FnDef {
-                start: compiled.len() + 2, // + 2 to jump over the future adress value && ret instruction
-                argc,
-            },
-        );
-        fns.push(HashMap::new()); // create inner scope
-        draw(tokens, ptr, fns, compiled); // get rid of body
-        compiled.insert(target, ByteCodePoint::i(compiled.len() as i64 + 1)); // we now know how big the fn is, and so can insert code to skip to the end
-        compiled.insert(target, ByteCodePoint::c(Instruction::RET)); // make sure we return out when called, push second to shift stuff over
-        fns.pop(); // delete inner scope
-        return;
+    match tok.as_str() {
+        "fn" => {
+            let fn_name = next_tok(tokens, ptr);
+            let argc = next_tok(tokens, ptr).parse::<usize>().unwrap();
+            compiled.push(ByteCodePoint::c(Instruction::JZ));
+            compiled.insert(compiled.len(), ByteCodePoint::i(0));
+            let target = compiled.len();
+			let goal = compiled.len() + 1;
+			println!("{goal:?}");
+            fns.last_mut().unwrap().insert(
+                fn_name.clone(),
+                FnDef {
+                    start: compiled.len() + 1, // + 1 to jump over the future adress value instruction and land on good val
+                    argc,
+                },
+            );
+            fns.push(HashMap::new()); // create inner scope
+            draw(tokens, ptr, fns, compiled); // get rid of body
+            compiled.push(ByteCodePoint::c(Instruction::RET)); // make sure we return out when called, push second to shift stuff over
+            compiled.insert(target, ByteCodePoint::i(compiled.len() as i64 + 1)); // we now know how big the fn is, and so can insert code to skip to the end
+            fns.pop(); // delete inner scope
+        }
+        "sub" => {
+            compiled.push(ByteCodePoint::c(Instruction::SUB));
+            draw_many(tokens, ptr, 2, fns, compiled);
+        }
+        "div" => {
+            compiled.push(ByteCodePoint::c(Instruction::DIV));
+            draw_many(tokens, ptr, 2, fns, compiled);
+        }
+        "do" => {
+            let count = next_tok(tokens, ptr).parse().unwrap();
+            draw_many(tokens, ptr, count, fns, compiled);
+        }
+        "set" => {
+            compiled.push(ByteCodePoint::c(Instruction::SET));
+            draw(tokens, ptr, fns, compiled); // get the value for it, does this even work though?
+        }
+        "ref" => {
+            compiled.push(ByteCodePoint::c(Instruction::REF));
+            draw(tokens, ptr, fns, compiled);
+        }
+        "print" => {
+            compiled.push(ByteCodePoint::c(Instruction::PRINT));
+            draw(tokens, ptr, fns, compiled);
+        }
+        _ => {
+            let mut argc: Option<&FnDef> = None;
+            for scope in fns.iter().rev() {
+                match scope.get(tok) {
+                    None => {}
+                    Some(v) => {
+                        argc = Some(v);
+                        break;
+                    }
+                }
+            }
+            if let Some(def) = argc {
+				compiled.push(ByteCodePoint::c(Instruction::CALL));
+				compiled.push(ByteCodePoint::i(def.start as i64));
+				compiled.push(ByteCodePoint::i(def.argc as i64));
+                draw_many(tokens, ptr, def.argc, fns, compiled);
+                return;
+            }
+            if let Ok(val) = tok.parse::<i64>() {
+                compiled.push(ByteCodePoint::i(val));
+                return;
+            }
+            if let Ok(val) = tok.parse::<f64>() {
+                compiled.push(ByteCodePoint::f(val));
+                return;
+            }
+			println!("{tok:?}");
+            compiled.push(ByteCodePoint::s(tok.to_string()));
+        }
     }
-    if tok == "sub" {
-		compiled.push(ByteCodePoint::c(Instruction::SUB));
-		draw_many(tokens, ptr, 2, fns, compiled);
-		return;
-	}
-	if tok == "div" {
-		compiled.push(ByteCodePoint::c(Instruction::DIV));
-		draw_many(tokens, ptr, 2, fns, compiled);
-		return;
-	}
-	if tok == "do" {
-		let count = next_tok(tokens, ptr).parse().unwrap();
-		draw_many(tokens, ptr, count, fns, compiled);
-		return;
-	}
-	if tok == "set" {
-		compiled.push(ByteCodePoint::c(Instruction::SET));
-		let var = next_tok(tokens, ptr); // what about &mut ?
-		compiled.push(ByteCodePoint::s(var.to_string()));
-		draw(tokens, ptr, fns, compiled); // get the value for it
-	}
 }
 
 fn draw_many(
@@ -120,8 +157,11 @@ fn main() {
     for char in s.chars() {
         // println!("{escaped} {commented} {char} {chunks:?}");
         if escaped {
-            *tokens.last_mut().unwrap() += &char.to_string();
             escaped = false;
+            if commented {
+                continue;
+            }
+            *tokens.last_mut().unwrap() += &char.to_string();
             continue;
         }
         if char == '\\' {
